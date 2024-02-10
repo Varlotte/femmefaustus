0480786431a644e5ab7d3b68be54327c1ed2dbd1
Charlotte Bush <clangbush@gmail.com>
2024-02-09 20:26:04 -0500

    basic routing set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,22 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import Home from "./pages/home";
+import Nav from "./components/nav";
+import { BrowserRouter, NavLink } from "react-router-dom";
+import Social from "./components/social";
+import Contact from "./components/contact";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
-  //put nav routing here
+export default function App() {
+  // const year = new Date().getFullYear();
+  // const [count, setCount] = useState(0);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <Nav />
+      <Home />
+      {/* <Social /> */}
+      <Contact />
+    </BrowserRouter>
   );
 }
-
-export default App;
